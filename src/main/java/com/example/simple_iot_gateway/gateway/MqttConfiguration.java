3e89df3262a188a6282f7bda70323db57fac8cf5
TorsteinM <torsteinem@gmail.com>
2023-04-23 17:08:43 +0200

    refactor: moving mqtt config
    Moving mqtt config out of code.

--- src/main/java/com/example/simple_iot_gateway/gateway/MqttConfiguration.java
+++ src/main/java/com/example/simple_iot_gateway/gateway/MqttConfiguration.java
@@ -3,17 +3,25 @@ package com.example.simple_iot_gateway.gateway;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
 
 @Configuration
+@PropertySource("classpath:secret.properties")
 public class MqttConfiguration {
+
+    @Value("${mqtt.broker.url}")
+    String serverURI;
+    @Value("${mqtt.username}")
+    String clientId;
+    @Value("${mqtt.password}")
+    String clientPass;
 
     @Bean
     public MqttClient mqttClient() throws Exception {
-        String serverURI = "tcp://192.168.0.56:1883";
-        String clientId = "mqtt";
-        String clientPass = "mqtt!pass";
         MqttClient mqttClient = new MqttClient(serverURI, clientId);
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
